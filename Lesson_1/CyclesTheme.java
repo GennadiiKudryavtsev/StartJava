bb3a4f12ab602785ecaf908ee131e086d03836af
Kudryavtsev Gennady <gennadykudryavtsev@MacBook-Pro-Gennadij.local>
2023-01-22 19:20:53 +0300

    Home Work cycles

--- Lesson_1/CyclesTheme.java
+++ Lesson_1/CyclesTheme.java
@@ -6,13 +6,13 @@ public class CyclesTheme {
         int sumEvenNumbers = 0;
         int sumОddNumbers = 0;
         do {
-            if (counter % 2== 0) {
+            if (counter % 2 == 0) {
                 sumEvenNumbers += counter;
             } else {
                 sumОddNumbers += counter;
             }
             counter++;
-        } while(counter <= 21);
+        } while (counter <= 21);
         System.out.println("В промежутке [-10, 21] сумма четных чисел = " + sumEvenNumbers
                 + ", а нечетных = " + sumОddNumbers);
 
@@ -20,37 +20,30 @@ public class CyclesTheme {
         int num1 = 10;
         int num2 = 5;
         int num3 = -1;
-        if (num2 > num3) {
-            int num4 = num2;
-            num2 = num3;
-            num3 = num4;
-        }
-        if (num1 > num3) {
-            int num4 = num1;
-            num1 = num3;
-            num3 = num4;
-        }
-        if (num1 > num2) {
-            int num4 = num1;
-            num1 = num2;
-            num2 = num4;
-        }
+        int min = num2;
         int max = num3;
-        int average = num2;
-        int min = num1;
+        if (num2 > num3) {
+            min = num3;
+            max = num2;
+        }
+        if (num1 < min) {
+            min = num1;
+        }
+        if (num1 > max) {
+            max = num1;
+        }
         System.out.println("Максимальное число = " + max + ". Минимальное число = " + min);
         for (int i = max - 1; i > min; i--) {
             System.out.print(i + " ");
         }
-        System.out.println();
 
-        System.out.println("\n3. Вывод реверсивного числа и суммы его цифр\n");
-        num2 = 1234;
+        System.out.println("\n\n3. Вывод реверсивного числа и суммы его цифр\n");
+        int num = 1234;
         int sum = 0;
         System.out.print("Исходное число: ");
-        while (num2 > 0) {
-            int digit = num2 % 10;
-            num2 /= 10;
+        while (num > 0) {
+            int digit = num % 10;
+            num /= 10;
             sum += digit;
             System.out.print(digit);
         }
@@ -60,25 +53,23 @@ public class CyclesTheme {
         counter = 0;
         max = 24;
         for (int i = 1; i < max; i += 2) {
-            if (i < max) {
-                System.out.printf("%5d", i);
-            }
+            System.out.printf("%5d", i);
             counter++;
             if (counter % 5 == 0) {
                 System.out.println();
                 counter = 0;
             }
         }
-        if (counter != 0) {
+        if (counter > 0) {
             int result = 5 - counter;
-            for (int j = 1; j <= result; j++) {
+            for (int i = 1; i <= result; i++) {
                 System.out.printf("%5d", 0);
             }
             System.out.println();
         }
 
         System.out.println("\n5. Проверка количества двоек на четность/нечетность\n");
-        int num = 3242592;
+        num = 3242592;
         int copyNum = num;
         int countTwos = 0;
         while (copyNum != 0) {
@@ -92,7 +83,7 @@ public class CyclesTheme {
         if (countTwos % 2 != 0) {
             type = " (нечетное) ";
         }
-        System.out.printf("Число " + num + " содержит " + countTwos + type + "количество двоек\n");
+        System.out.println("Число " + num + " содержит " + countTwos + type + "количество двоек");
 
         System.out.println("\n6. Отображение фигур в консоли\n");
         for (int i = 1; i <= 50; i++) {
@@ -103,46 +94,45 @@ public class CyclesTheme {
         }
 
         System.out.println();
-        int maxСolumn = 5;
-        while (maxСolumn >= 1) {
-            int line = 1;
-            while (line <= maxСolumn) {
+        int countColumn = 5;
+        int countSymbol = 1;
+        while (countColumn >= 1) {
+            countSymbol = 1;
+            while (countSymbol <= countColumn) {
                 System.out.print("#");
-                line++;
+                countSymbol++;
             }
             System.out.println();
-            maxСolumn--;
+            countColumn--;
         }
 
         System.out.println();
-        int line = 1;
-        int maxLine = 1;
-        int column = 0;
+        countSymbol = 1;
+        countColumn = 1;
         do {
-            column = 0;
+            int column = 0;
             do {
                 System.out.print("$");
                 column++;
-            } while (line > column);
-            if (maxLine < 3) {
-                line++;
+            } while (countSymbol > column);
+            if (countColumn < 3) {
+                countSymbol++;
             } else {
-                line--;
+                countSymbol--;
             }
-            maxLine++;
+            countColumn++;
             System.out.println();
-        } while (maxLine <= 5);
+        } while (countColumn <= 5);
+
 
         System.out.println("\n7. Отображение ASCII-символов\n");
         System.out.printf("%6s%6s%n", "Dec", "Char");
-        for (int i = 33; i <= 122; i++) {
-            if (i >= 48 && i <= 96) {
-                continue;
-            } else if (i % 2 == 0) {
-                if (i >= 'a' && i <= 'z') {
+        for (int i = 1; i <= 122; i++) {
+            if (i % 2 != 0) {
+                if (i <= 47) {
                     System.out.printf("%5d%5c%n", i, i);
                 }
-            } else if (i <= 47) {
+            } else if (i >= 'a' && i <= 'z') {
                 System.out.printf("%5d%5c%n", i, i);
             }
         }
@@ -166,7 +156,7 @@ public class CyclesTheme {
         System.out.println("\n9. Определение, является ли число счастливым\n");
         num = 123111;
         num2 = num;
-        column = 1;
+        int column = 1;
         int sum1 = 0;
         int sum2 = 0;
         num3 = num % 1000;
