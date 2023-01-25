d57649332edb9f3661dcf2a397f13cb8683da945
Kudryavtsev Gennady <gennadykudryavtsev@MacBook-Pro-Gennadij.local>
2023-01-25 10:48:46 +0300

    Home Work WolfTest2 after modification

--- Lesson_2/WolfTest.java
+++ Lesson_2/WolfTest.java
@@ -1,13 +1,25 @@
 public class WolfTest {
 
     public static void main(String[] args) {
-        Wolf wolf = new Wolf("female", "Barbara", 50, 5, "grey");
-        System.out.println(wolf.gender + ", " + wolf.nickname + ", "
-                + wolf.weight + ", " + wolf.age + ", " + wolf.color);
+        Wolf wolf = new Wolf("female", "Barbara", 50, 5, "grey");   
         wolf.go();
         wolf.sit();
         wolf.run();
         wolf.howl();
         wolf.hunt();
+
+        wolf.setGender("male");
+        wolf.setNickname("Boris");
+        wolf.setWeight(60);
+        wolf.setAge(12);
+        wolf.setColor("white");
+
+        wolf.getGender();
+        wolf.getNickname();
+        wolf.getWeight();
+        wolf.getAge();
+        wolf.getColor();
+        System.out.println(wolf.getGender() + ", " + wolf.getNickname() + ", " 
+                + wolf.getWeight() + ", " + wolf.getAge() + ", " + wolf.getColor());
     }
 }
