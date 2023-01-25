2bbca7fc74c7ef66abc140f95bbd653371e7ab95
Kudryavtsev Gennady <gennadykudryavtsev@MacBook-Pro-Gennadij.local>
2023-01-25 10:48:09 +0300

    Home Work Wolf2 after modification

--- Lesson_2/Wolf.java
+++ Lesson_2/Wolf.java
@@ -1,10 +1,10 @@
 public class Wolf {
 
-    String gender;
-    String nickname;
-    int weight;
-    int age;
-    String color;
+    private String gender;
+    private String nickname;
+    private int weight;
+    private int age;
+    private String color;
 
     public Wolf(String gender, String nickname, int weight, int age, String color) {
         this.gender = gender;
@@ -12,6 +12,51 @@ public class Wolf {
         this.weight = weight;
         this.age = age;
         this.color = color; 
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age > 8) {
+            System.out.println("Некорректный возраст");
+        } else {
+            age = age;
+        }
+        this.age = age;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
     }
 
     public void go() {
